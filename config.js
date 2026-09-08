e8f46839e2239fe1e17ec0b47a2990c38d6562dc
george31GA31 <157766840+george31GA31@users.noreply.github.com>
2026-09-08 19:26:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -13,6 +13,6 @@
 */
 window.NETT_HIER_CONFIG = {
   supabaseUrl: "https://ukkvukdzmdzpertbjnp.supabase.co",
-  supabasePublishableKey: "https://ukkvukdzmdzpertbjjnp.supabase.co",
+  supabasePublishableKey: "sb_publishable_Pp6-YdU3lhgflNrvF7wN1g_cnuBLpvt",
   photoBucket: "sticker-photos"
 };
